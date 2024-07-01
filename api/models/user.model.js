5636cffe23a82b40aa940a8daf2458e2c988fa69
Dilukshan <Dviyapury@gmail.com>
2024-07-01 17:50:09 +0530

    format

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true, //every one has to have a different user name
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true, //every one has to have a different user name
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, //every one has to have a different user name
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true, //every one has to have a different user name
-  },
-  password: {
-    type: String,
-    required: true,
-  }
-},{timestamps: true});
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
